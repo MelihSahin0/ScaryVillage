4e2ed9ced0dfbf59eaf929b5ccae455fe490610e
Melih Sahin <msa1412@students.fhv.at>
2024-05-23 15:38:02 +0200

    Viteconfig

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 import prettyCssModules from 'vite-plugin-pretty-css-modules';
 
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  },
   plugins: [react(), prettyCssModules()]
 });
