c77ffafc564735e23991e55f9af54ecc022a228b
Melih Sahin <msa1412@students.fhv.at>
2024-05-27 18:23:38 +0200

    fixing voicchat for linux

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,10 @@ import prettyCssModules from 'vite-plugin-pretty-css-modules';
 
 export default defineConfig({
   server: {
+    https: {
+      key: '/home/student/key.pem',
+      cert: '/home/student/cert.pem',
+    },
     host: '0.0.0.0',
     port: 5173
   },
